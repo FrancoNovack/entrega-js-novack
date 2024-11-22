e49bca6669564af75fe795fd4c1a616605e61211
Franco Novack <franconovack5@gmail.com>
2024-11-21 23:25:59 -0300

    entregaF

---
+++ inicioSecion.js
@@ -0,0 +1,28 @@
+const botonIncio = document.getElementById('botonInicio');
+
+const usuarioAutorizado = "usuario";
+const passwordAutorizada = '1234';
+
+botonIncio.addEventListener('click', ()=>{
+    Swal.fire({
+        title: "Inicio de sesion",
+        html: `
+            <input type="text" id="usuario" id="swal2-input" placeholder="usuario">
+            <input type="text" id="contraseña" id="swal2-input" placeholder="contraseña">
+        `,
+        confirmButtonText:"enviar",
+        cancelButtonText:"cancelar"
+    }).then((result)=>{
+        if(result.isConfirmed){
+            const usuario1 = document.getElementById('usuario').value;
+            const clave = document.getElementById('contraseña').value; 
+
+            if(usuario1 === usuarioAutorizado && clave === passwordAutorizada){
+                window.location.href= "/contenido.html"
+            }else{
+                alert("clave incorrecta")
+            }
+        }
+    })
+
+})
